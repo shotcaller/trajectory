b7ba9c333e38ea2f68e7b65514be48476df68e35
paramSonawane <paramsonawane@gmail.com>
2020-07-29 09:24:02 +0530

    moved the main canvas handlers to globas scope

--- js/script.js
+++ js/script.js
@@ -1,74 +1,83 @@
-let leftCanvas = new p5((lCanvas) => {
-    let lup = false;
+// let postProcessing = new postProcCanvas();
+let nPts, postProc, leftCanvas, rightCanvas;
 
-    lCanvas.setup = function(){
+leftCanvas = new p5(
+    (lCanvas) => {
+        lCanvas.setup = function(){
+            let canvWidth = 900
+            let canvHeight = 850
 
-        let canvWidth = 900
-        let canvHeight = 850
+            canvas = lCanvas.createCanvas(canvWidth, canvHeight);
 
-        canvas = lCanvas.createCanvas(canvWidth, canvHeight);
+            canvas.position(0, 0)
+            initMap(canvas);
 
-        // background(200)
+            nPts = new newPoints()
+            nPts.showTimeSelection();
+            nPts.showInputBox();
+            nPts.showAnimateButton();
 
-        // area1 = new searchArea(320, 320, 300, 10)
-        // area1.generateFoVs()
-        
-        canvas.position(0, 0)
-        initMap(canvas);
+            // nPts.showExportButton();
 
-        nPts = new newPoints()
-        nPts.showTimeSelection();
-        nPts.showInputBox();
-        nPts.showAnimateButton();
-        // nPts.showExportButton();
+            myMap.onChange(()=>{
+                nPts.plotPoints()
+            });
 
-        myMap.onChange(()=>{
-            nPts.plotPoints()
-        });
+            lCanvas.frameRate(120);
+            lCanvas.fill(200, 100, 100);
+        }
 
+        lCanvas.draw = function(){
 
-        lCanvas.frameRate(120);
-        lCanvas.fill(200, 100, 100);
-        // noLoop();
+        }
+
+        function initMap(canvas){
+            myMap = new Mappa('Leaflet').tileMap({
+                lat : jsonData.lat[0],
+                lng : jsonData.lon[0],
+                zoom : 9,
+                style : "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            });
+
+            myMap.overlay(canvas);
+        }
     }
+);
 
-    lCanvas.draw = function(){
-        // // clear()
-        // area1.FoVs.forEach(x=>{x.forEach(y=>{y.draw()})});
-        // area1.draw();
-        // area1.drawBoundary();
+rightCanvas = new p5(
+    (rCanvas) => {
+        let lup = false;
+
+        rCanvas.setup = function(){
+            let  canvWidth = 900, canvHeight = 850;
+            canvas = rCanvas.createCanvas(canvWidth, canvHeight)
+            canvas.position(950, 10)
+            rCanvas.background(200)
+
+            area1 = new searchArea(320, 320, 300, 10)
+            area1.generateFoVs()
+            zz = new zigZagPath(100, 100, 200, 10, 40, 35);
+
+            postProc = new postProcCanvas()
+            postProc.showExportButton()
+
+            rCanvas.noLoop();
+        }
+
+        rCanvas.draw = function(){
+            rCanvas.clear()
+            area1.FoVs.forEach(x=>{x.forEach(y=>{y.draw()})});
+            area1.draw();
+            area1.drawBoundary();
+            zz.draw();
+        }
+
+        function mousePressed(){
+            // lup = !lup
+            // if(lup)
+            // loop()
+            // else noLoop()
+            // area1.updateProbabilities(mouseX, mouseY, 1, 0.05)
+        }
     }
-
-    function initMap(canvas){
-        myMap = new Mappa('Leaflet').tileMap({
-            lat : jsonData.lat[0],
-            lng : jsonData.lon[0],
-            zoom : 9,
-            style : "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        });
-
-        myMap.overlay(canvas);
-    }
-
-    function mousePressed(){
-        // lup = !lup
-        // if(lup)
-        // loop()
-        // else noLoop()
-        // area1.updateProbabilities(mouseX, mouseY, 1, 0.05)
-
-    }
-})
-
-let rightCanvas = new p5((rCanvas) => {
-    rCanvas.setup = function(){
-        let  canvWidth = 900, canvHeight = 850;
-        canvas = rCanvas.createCanvas(canvWidth, canvHeight)
-        canvas.position(950, 10)
-        rCanvas.background(200)
-    }
-
-    rCanvas.draw = function(){
-
-    }
-})
+);
